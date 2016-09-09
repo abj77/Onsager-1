e4b04fed422740bb24136e69fbaadcb4327903cf
Abhinav Jain <ahbinavjain77@gmail.com>
2016-09-09 08:00:41 -0500

    Pulled tests for vector stars. Need to modify them and create some unique ones for meta classes

--- test/test_crystalStarsMeta.py
+++ test/test_crystalStarsMeta.py
@@ -12,7 +12,17 @@ import numpy as np
 import onsager.crystalStars as stars
 
 
-# Setup for simple cubic, and FCC cells:
+def setuportho():
+    crys = crystal.Crystal(np.array([[3, 0, 0], [0, 2, 0], [0, 0, 1]], dtype=float),
+                           [[np.zeros(3)]])
+    jumpnetwork = [[((0, 0), np.array([3., 0., 0.])), ((0, 0), np.array([-3., 0., 0.]))],
+                   [((0, 0), np.array([0., 2., 0.])), ((0, 0), np.array([0., -2., 0.]))],
+                   [((0, 0), np.array([0., 0., 1.])), ((0, 0), np.array([0., 0., -1.]))]]
+    return crys, jumpnetwork
+
+
+def orthorates():
+    return np.array([3., 2., 1.])
 
 def setupcubic():
     crys = crystal.Crystal(np.eye(3), [[np.zeros(3)]])
@@ -145,6 +155,228 @@ class JumpNetworkTests(unittest.TestCase):
                 for i, ((ji, jf), dx) in enumerate(jumplist):
                     self.assertTrue(np.allclose(dx, w0list[i].dx))
 
+class VectorStarTests(unittest.TestCase):
+    """Set of tests that our VectorStar class is behaving correctly"""
+    longMessage = False
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setuportho()
+        self.chem = 0
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+    def testVectorStarGenerate(self):
+        """Can we generate star-vectors that make sense?"""
+        self.starset.generate(1)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        self.assertTrue(self.vecstarset.Nvstars > 0)
+
+    def VectorStarConsistent(self, nshells):
+        """Do the star vectors obey the definition?"""
+        self.starset.generate(nshells)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        for s, vec in zip(self.vecstarset.vecpos, self.vecstarset.vecvec):
+            for si, v in zip(s, vec):
+                PS = self.starset.states[si]
+                for g in self.crys.G:
+                    gsi = self.starset.stateindex(PS.g(self.crys, self.chem, g))
+                    vrot = self.crys.g_direc(g, v)
+                    for si1, v1 in zip(s, vec):
+                        if gsi == si1: self.assertTrue(np.allclose(v1, vrot))
+
+    def VectorStarOrthonormal(self, nshells):
+        """Are the star vectors orthonormal?"""
+        self.starset.generate(1)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        for s1, v1 in zip(self.vecstarset.vecpos, self.vecstarset.vecvec):
+            for s2, v2 in zip(self.vecstarset.vecpos, self.vecstarset.vecvec):
+                if s1[0] == s2[0]:
+                    dp = sum(np.dot(vv1, vv2) for vv1, vv2 in zip(v1, v2))
+                    if np.allclose(v1[0], v2[0]):
+                        self.assertAlmostEqual(1., dp,
+                                               msg='Failed normality for {}/{} and {}/{}'.format(
+                                                   self.starset.states[s1[0]], v1[0],
+                                                   self.starset.states[s2[0]], v2[0]))
+                    else:
+                        self.assertAlmostEqual(0., dp,
+                                               msg='Failed orthogonality for {}/{} and {}/{}'.format(
+                                                   self.starset.states[s1[0]], v1[0],
+                                                   self.starset.states[s2[0]], v2[0]))
+
+    def testVectorStarConsistent(self):
+        """Do the star vectors obey the definition?"""
+        self.VectorStarConsistent(2)
+
+    def testVectorStarOrthonormal(self):
+        self.VectorStarOrthonormal(2)
+
+    def testVectorStarCount(self):
+        """Does our star vector count make any sense?"""
+        self.starset.generate(1)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        self.assertEqual(self.vecstarset.Nvstars, 3)
+
+    def testVectorStarOuterProduct(self):
+        """Do we generate the correct outer products for our star-vectors (symmetry checks)?"""
+        self.starset.generate(2)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        self.assertEqual(np.shape(self.vecstarset.outer),
+                         (3, 3, self.vecstarset.Nvstars, self.vecstarset.Nvstars))
+        # check our diagonal blocks first:
+        for outer in [self.vecstarset.outer[:, :, i, i]
+                      for i in range(self.vecstarset.Nvstars)]:
+            self.assertAlmostEqual(np.trace(outer), 1)
+            # should also be symmetric:
+            for g in self.crys.G:
+                g_out_gT = self.crys.g_tensor(g, outer)
+                self.assertTrue(np.allclose(g_out_gT, outer))
+        # off-diagonal terms now
+        for outer in [self.vecstarset.outer[:, :, i, j]
+                      for i in range(self.vecstarset.Nvstars)
+                      for j in range(self.vecstarset.Nvstars)
+                      if i != j]:
+            self.assertAlmostEqual(np.trace(outer), 0)
+            # should also be symmetric:
+            for g in self.crys.G:
+                g_out_gT = self.crys.g_tensor(g, outer)
+                self.assertTrue(np.allclose(g_out_gT, outer))
+        for i, (s0, svv0) in enumerate(zip(self.vecstarset.vecpos, self.vecstarset.vecvec)):
+            for j, (s1, svv1) in enumerate(zip(self.vecstarset.vecpos, self.vecstarset.vecvec)):
+                testouter = np.zeros((3, 3))
+                if s0[0] == s1[0]:
+                    # we have the same underlying star to work with, so...
+                    for v0, v1 in zip(svv0, svv1):
+                        testouter += np.outer(v0, v1)
+                self.assertTrue(np.allclose(self.vecstarset.outer[:, :, i, j], testouter),
+                                msg='Failed for vector stars {} and {}:\n{} !=\n{}'.format(
+                                    i, j, self.vecstarset.outer[:, :, i, j], testouter))
+
+
+class VectorStarFCCTests(VectorStarTests):
+    """Set of tests that our VectorStar class is behaving correctly, for FCC"""
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setupFCC()
+        self.chem = 0
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+    def testVectorStarCount(self):
+        """Does our star vector count make any sense?"""
+        self.starset.generate(2)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        # nn + nn = 4 stars, and that should make 5 star-vectors!
+        self.assertEqual(self.starset.Nstars, 4)
+        self.assertEqual(self.vecstarset.Nvstars, 5)
+
+    def testVectorStarConsistent(self):
+        """Do the star vectors obey the definition?"""
+        self.VectorStarConsistent(2)
+
+    def testVectorStarOuterProductMore(self):
+        """Do we generate the correct outer products for our star-vectors?"""
+        self.starset.generate(2)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        # with cubic symmetry, these all have to equal 1/3 * identity, and
+        # with a diagonal matrix
+        testouter = 1. / 3. * np.eye(3)
+        for outer in [self.vecstarset.outer[:, :, i, i]
+                      for i in range(self.vecstarset.Nvstars)]:
+            self.assertTrue(np.allclose(outer, testouter))
+        for outer in [self.vecstarset.outer[:, :, i, j]
+                      for i in range(self.vecstarset.Nvstars)
+                      for j in range(self.vecstarset.Nvstars)
+                      if i != j]:
+            self.assertTrue(np.allclose(outer, 0))
+
+
+class VectorStarHCPTests(VectorStarTests):
+    """Set of tests that our VectorStar class is behaving correctly, for HCP"""
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setupHCP()
+        self.chem = 0
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+    def testVectorStarCount(self):
+        """Does our star vector count make any sense?"""
+        self.starset.generate(1)
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        # two stars, with two vectors: one basal, one along c (more or less)
+        self.assertEqual(self.starset.Nstars, 2)
+        self.assertEqual(self.vecstarset.Nvstars, 2 + 2)
+
+
+class VectorStarOmega0Tests(unittest.TestCase):
+    """Set of tests for our expansion of omega_0"""
+
+    longMessage = False
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setuportho()
+        self.rates = orthorates()
+        self.chem = 0
+        self.sitelist = self.crys.sitelist(self.chem)
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+    def testConstructOmega0(self):
+        # NOTE: now we only take omega0 *here* to be those equivalent to omega1 jumps; the exchange
+        # terms are handled in omega2; the jumps outside the kinetic shell simply contributed onsite escape
+        # terms that get subtracted away, since the outer kinetic shell *has* to have zero energy
+        self.starset.generate(2)  # we need at least 2nd nn to even have jump networks to worry about...
+        jumpnetwork_omega1, jt, sp = self.starset.jumpnetwork_omega1()
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        rate0expand, rate0escape, rate1expand, rate1escape = self.vecstarset.rateexpansions(jumpnetwork_omega1, jt)
+        # rate0expand = self.vecstarset.rate0expansion()
+        self.assertEqual(np.shape(rate0expand),
+                         (self.vecstarset.Nvstars, self.vecstarset.Nvstars, len(self.jumpnetwork)))
+        om0expand = self.rates.copy()
+        # put together the onsite and off-diagonal terms for our matrix:
+        # go through each state, and add the escapes for the vacancy; see if vacancy (PS.j)
+        # is the initial state (i) for a transition out (i,f), dx
+        om0matrix = np.zeros((self.starset.Nstates, self.starset.Nstates))
+        for ns, PS in enumerate(self.starset.states):
+            for rate, jumplist in zip(self.rates, self.starset.jumpnetwork_index):
+                for TS in [self.starset.jumplist[jumpindex] for jumpindex in jumplist]:
+                    if PS.j == TS.i:
+                        nsend = self.starset.stateindex(PS + TS)
+                        if nsend is not None:
+                            om0matrix[ns, nsend] += rate
+                            om0matrix[ns, ns] -= rate
+        # now, we need to convert that omega0 matrix into the "folded down"
+        for i, (sRv0, svv0) in enumerate(zip(self.vecstarset.vecpos, self.vecstarset.vecvec)):
+            for j, (sRv1, svv1) in enumerate(zip(self.vecstarset.vecpos, self.vecstarset.vecvec)):
+                om0_sv = 0
+                for i0, v0 in zip(sRv0, svv0):
+                    for i1, v1 in zip(sRv1, svv1):
+                        om0_sv += np.dot(v0, v1) * om0matrix[i0, i1]
+                om0_sv_comp = np.dot(rate0expand[i, j], om0expand)
+                if i == j: om0_sv_comp += np.dot(rate0escape[i], om0expand)
+                self.assertAlmostEqual(om0_sv, om0_sv_comp,
+                                       msg='Failed to match {}, {}: {} != {}'.format(
+                                           i, j, om0_sv, om0_sv_comp))
+
+
+class VectorStarFCCOmega0Tests(VectorStarOmega0Tests):
+    """Set of tests for our expansion of omega_0 for FCC"""
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setupFCC()
+        self.rates = FCCrates()
+        self.chem = 0
+        self.sitelist = self.crys.sitelist(self.chem)
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+
+class VectorStarHCPOmega0Tests(VectorStarOmega0Tests):
+    """Set of tests for our expansion of omega_0 for HCP"""
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setupHCP()
+        self.rates = HCPrates()
+        self.chem = 0
+        self.sitelist = self.crys.sitelist(self.chem)
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+
 
 class HCPMetaTests(unittest.TestCase):
     """Set of tests specific to the HCP lattice with metastable states."""
@@ -161,3 +393,103 @@ class HCPMetaTests(unittest.TestCase):
         print(self.starset.states)
         for s in self.starset.states:
             self.assertTrue(s.i not in self.meta_sites)
+
+class VectorStarOmegalinearTests(unittest.TestCase):
+    """Set of tests for our expansion of omega_1"""
+
+    longMessage = False
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setuportho()
+        # self.rates = orthorates()
+        self.chem = 0
+        self.sitelist = self.crys.sitelist(self.chem)
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+    def testConstructOmega1(self):
+        self.starset.generate(2)  # we need at least 2nd nn to even have jump networks to worry about...
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        jumpnetwork_omega1, jt, sp = self.starset.jumpnetwork_omega1()
+        rate0expand, rate0escape, rate1expand, rate1escape = self.vecstarset.rateexpansions(jumpnetwork_omega1, jt)
+        # rate1expand = self.vecstarset.rate1expansion(jumpnetwork)
+        self.assertEqual(np.shape(rate1expand),
+                         (self.vecstarset.Nvstars, self.vecstarset.Nvstars, len(jumpnetwork_omega1)))
+        # make some random rates
+        om1expand = np.random.uniform(0, 1, len(jumpnetwork_omega1))
+        for i in range(self.vecstarset.Nvstars):
+            for j in range(self.vecstarset.Nvstars):
+                # test the construction
+                om1 = 0
+                for Ri, vi in zip(self.vecstarset.vecpos[i], self.vecstarset.vecvec[i]):
+                    for Rj, vj in zip(self.vecstarset.vecpos[j], self.vecstarset.vecvec[j]):
+                        for jumplist, rate in zip(jumpnetwork_omega1, om1expand):
+                            for (IS, FS), dx in jumplist:
+                                if IS == Ri:
+                                    if IS == Rj: om1 -= np.dot(vi, vj) * rate  # onsite terms...
+                                    if FS == Rj: om1 += np.dot(vi, vj) * rate
+                om1_sv_comp = np.dot(rate1expand[i, j], om1expand)
+                if i == j: om1_sv_comp += np.dot(rate1escape[i], om1expand)
+                self.assertAlmostEqual(om1, om1_sv_comp,
+                                       msg='Failed to match {}, {}: {} != {}'.format(
+                                           i, j, om1, om1_sv_comp))
+                # print(np.dot(rateexpand, om1expand))
+
+
+class VectorStarFCCOmegalinearTests(VectorStarOmegalinearTests):
+    """Set of tests for our expansion of omega_1 for FCC"""
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setupFCC()
+        # self.rates = FCCrates()
+        self.chem = 0
+        self.sitelist = self.crys.sitelist(self.chem)
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+class VectorStarHCPOmegalinearTests(VectorStarOmegalinearTests):
+    """Set of tests for our expansion of omega_1 for HCP"""
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setupHCP()
+        # self.rates = HCPrates()
+        self.chem = 0
+        self.sitelist = self.crys.sitelist(self.chem)
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+class VectorStarOmega2linearTests(unittest.TestCase):
+    """Set of tests for our expansion of omega_2"""
+
+    longMessage = False
+
+    def setUp(self):
+        self.crys, self.jumpnetwork = setuportho()
+        self.rates = orthorates()
+        self.chem = 0
+        self.sitelist = self.crys.sitelist(self.chem)
+        self.starset = stars.StarSet(self.jumpnetwork, self.crys, self.chem)
+
+    def testConstructOmega2(self):
+        self.starset.generate(2)  # we need at least 2nd nn to even have jump networks to worry about...
+        self.vecstarset = stars.VectorStarSet(self.starset)
+        jumpnetwork_omega2, jt, sp = self.starset.jumpnetwork_omega2()
+        rate0expand, rate0escape, rate2expand, rate2escape = self.vecstarset.rateexpansions(jumpnetwork_omega2, jt)
+
+        # construct the set of rates corresponding to the unique stars:
+        om2expand = self.rates.copy()
+        self.assertEqual(np.shape(rate2expand),
+                         (self.vecstarset.Nvstars, self.vecstarset.Nvstars, len(jumpnetwork_omega2)))
+        for i in range(self.vecstarset.Nvstars):
+            for j in range(self.vecstarset.Nvstars):
+                # test the construction
+                om2 = 0
+                for Ri, vi in zip(self.vecstarset.vecpos[i], self.vecstarset.vecvec[i]):
+                    for Rj, vj in zip(self.vecstarset.vecpos[j], self.vecstarset.vecvec[j]):
+                        for jumplist, rate in zip(jumpnetwork_omega2, om2expand):
+                            for (IS, FS), dx in jumplist:
+                                if IS == Ri:
+                                    if IS == Rj: om2 -= np.dot(vi, vj) * rate  # onsite terms...
+                                    if FS == Rj: om2 += np.dot(vi, vj) * rate
+                om2_sv_comp = np.dot(rate2expand[i, j], om2expand)
+                if i == j: om2_sv_comp += np.dot(rate2escape[i], om2expand)
+                self.assertAlmostEqual(om2, om2_sv_comp,
+                                       msg='Failed to match {}, {}: {} != {}'.format(
+                                           i, j, om2, om2_sv_comp))
